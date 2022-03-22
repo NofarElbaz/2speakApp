9470755852b1526a05bcaf9eab3f9285ac39d3f5
nofarElbaz <nofarelbaz44@gmail.com>
2022-03-22 14:17:27 +0200

    add voice recording

--- screens/RecordingScreen.js
+++ screens/RecordingScreen.js
@@ -1,93 +1,86 @@
 
-import {StyleSheet, View ,Button, Text} from 'react-native';
+import { View, StyleSheet } from 'react-native';
 import {HeadLine} from "../components/HeadLine";
 import { IconButton } from 'react-native-paper';
-import React , { useState, useEffect } from 'react'; 
+import React , { useState } from 'react'; 
+import { Audio } from 'expo-av';
+/*
 import Voice, {
   SpeechResultsEvent,
   SpeechErrorEvent,
 } from "@react-native-voice/voice";
-
+*/
 
 export const RecordingScreen = () => {
+  const [recording, setRecording] = useState();
 
-  const [results, setResults] = useState([]);
-  const [isListening, setIsListening] = useState(false);
-
-  useEffect(() => {
-    function onSpeechResults(e) {
-      setResults(e.value ?? []);
-    }
-    function onSpeechError(e) {
-      console.error(e);
-    }
-    Voice.onSpeechError = onSpeechError;
-    Voice.onSpeechResults = onSpeechResults;
-    return function cleanup() {
-      Voice.destroy().then(Voice.removeAllListeners);
-    };
-  }, []);
-
-  async function toggleListening() {
+  async function startRecording() {
     try {
-      if (isListening) {
-        await Voice.stop();
-        setIsListening(false);
-      } else {
-        setResults([]);
-        await Voice.start("he-IL");
-        setIsListening(true);
-      }
-    } catch (e) {
-      console.error(e);
+      console.log('Requesting permissions..');
+      await Audio.requestPermissionsAsync();
+      await Audio.setAudioModeAsync({
+        allowsRecordingIOS: true,
+        playsInSilentModeIOS: true,
+      }); 
+      console.log('Starting recording..');
+      const { recording } = await Audio.Recording.createAsync(
+         Audio.RECORDING_OPTIONS_PRESET_HIGH_QUALITY
+      );
+      setRecording(recording);
+      console.log('Recording started');
+    } catch (err) {
+      console.error('Failed to start recording', err);
     }
   }
 
-  return (
-    <View style={styles.container}>
-      <Text>Press the button and start speaking.</Text>
-      <Button
-        title={isListening ? "Stop Recognizing" : "Start Recognizing"}
-        onPress={toggleListening}
-      />
-      <Text>Results:</Text>
-      {results.map((result, index) => {
-        return <Text key={`result-${index}`}>{result}</Text>;
-      })}
-    </View>
-  );
-/*
-    return (
-      <View style = {style.container}>
-        <HeadLine/>
-        <View style={style.recordIconStack}>
-          <IconButton
-              icon="record"
-              color={"red"}
-              size={50}
-              onPress={() => console.log("start record")}
-          />
-          <IconButton
-              icon="stop"
-              color={"red"}
-              size={50}
-              onPress={() => console.log("stop record")}
-          />
-          <IconButton
-            icon="play"
-            color={"red"}
-            size={50}
-            onPress={() => console.log("play record")}
-          />
-        </View> 
-      </View> 
-    )
+  async function stopRecording() {
+    console.log('Stopping recording..');
+    //setRecording(recording);
+    await recording.stopAndUnloadAsync();
+    const uri = recording.getURI(); 
+    console.log('Recording stopped and stored at', uri);
+  }
 
-  */
+  async function playRecording() {
+    console.log("play recording...");
+    console.log( recording.getURI());
+    const { sound } = await Audio.Sound.createAsync(
+      {uri: recording.getURI() }
+    );
+    //setRecording(recording);
+    console.log("Playing Sound");
+    await sound.playAsync();
+  }
+
+  return (
+    <View style = {style.container}>
+      <HeadLine/>
+      <View style={style.recordIconStack}>
+        <IconButton
+          icon="record"
+          color={"red"}
+          size={50}
+          onPress={() => startRecording() }
+        />
+        <IconButton
+          icon="stop"
+          color={"red"}
+          size={50}
+          onPress={() => stopRecording ()}
+        />
+         <IconButton
+          icon="play"
+          color={"red"}
+          size={50}
+          onPress={() => playRecording()}
+        />
+      </View> 
+
+    </View> 
+  )
 }
 
-/*
-   const style = StyleSheet.create({
+  const style = StyleSheet.create({
     container: {
         flex :1 
     },
@@ -100,16 +93,5 @@ export const RecordingScreen = () => {
         marginLeft: "19%",
         flexDirection: 'row',
     },
-});
-
-*/
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-    backgroundColor: "#F5FCFF",
-  },
 });
 
